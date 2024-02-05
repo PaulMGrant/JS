3924b6474333330b275099ce7106b8b1aa304ceb
Paul Grant <Paul@paul-grant.co.uk>
2024-02-05 10:51:47 +0000

    first commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,9 @@
+// true;
+// console.log(true);
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Paul');
